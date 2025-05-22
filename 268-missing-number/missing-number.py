a6731da7cde5c4749efc8936b922e0273bc444e5
rbuenk <65895276+rbuenk@users.noreply.github.com>
2025-05-22 20:12:23 +0200

    Time: 4 ms (55.05%) | Memory: 14.1 MB (5.30%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        list_nums = list(set(nums))
+        lenghth = len(list_nums)
+        missing = 0
+
+        if list_nums[0] == 1:
+            return missing
+        
+        if list_nums[lenghth-1] == lenghth-1:
+            missing = lenghth
+            return missing
+
+        for i, val in enumerate(list_nums):
+            if i != val:
+                missing = val - 1  
+                return missing
