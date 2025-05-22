caee5fd98e8934f5359763476d7f40f32996a25b
rbuenk <65895276+rbuenk@users.noreply.github.com>
2025-05-22 20:25:27 +0200

    Time: 10 ms (39.21%) | Memory: 14.1 MB (5.30%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -4,28 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # list_nums = list(set(nums))
-        # lenghth = len(list_nums)
-        # missing = 0
+        list_nums = list(set(nums))
+        lenghth = len(list_nums)
+        missing = 0
 
-        # if list_nums[0] == 1:
-        #     return 0
+        if list_nums[0] == 1:
+            return 0
         
-        # if list_nums[lenghth-1] == lenghth-1:
-        #     return lenghth
+        if list_nums[lenghth-1] == lenghth-1:
+            return lenghth
 
-        # for i, val in enumerate(list_nums):
-        #     if i != val:
-        #         missing = val - 1  
-        #         return missing
+        for i, val in enumerate(list_nums):
+            if i != val:
+                missing = val - 1  
+                return missing
 
-        sum_i = 0
-        sum_v = 0
+        # sum_i = 0
+        # sum_v = 0
 
-        for i, v in enumerate(nums):
-            sum_i += i + 1
-            sum_v += v
+        # for i, v in enumerate(nums):
+        #     sum_i += i + 1
+        #     sum_v += v
 
-            print "Index ", i,  "Value ", v ,"sum_i = ", sum_i, "sum_v = ", sum_v
+        #     print "Index ", i,  "Value ", v ,"sum_i = ", sum_i, "sum_v = ", sum_v
         
-        return sum_i - sum_v
+        # return sum_i - sum_v
