81f8fc0c763d2a4bb1f6a0b9340aa5c876733776
rbuenk <65895276+rbuenk@users.noreply.github.com>
2025-05-23 23:44:45 +0200

    Time: 0 ms (100.00%) | Memory: 14 MB (75.78%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        r = 0
+        for n in nums:
+            r ^= n
+        return r
